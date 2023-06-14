eafc0975c764cfc133bce90db6b1a670dfde6dff
Rimsoo <abdeldebeche@gmail.com>
2023-06-15 01:23:58 +0200

    fixed close popup

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,11 +1,10 @@
-
 function openPopup(contentUrl) {
     var popup = document.getElementById('popup');
     if (popup) {
         popup.style.display = 'block';
-        document.body.style.overflow = 'hidden'; // Prevent scrolling of background content
+        document.body.style.overflow = 'hidden'; // Empêche le défilement du contenu arrière-plan
 
-        fetch(contentUrl) // Replace with the actual path to your HTML page
+        fetch(contentUrl) // Remplacez par le chemin réel vers votre page HTML
             .then(response => response.text())
             .then(html => {
                 var popupContent = popup.querySelector('#popup-html');
@@ -14,12 +13,26 @@ function openPopup(contentUrl) {
                 }
             });
     }
+    // Add a small delay before setting the outside click event
+    setTimeout(function() {
+        document.addEventListener('click', outsideClickHandler);
+    }, 100);
 }
 
 function closePopup() {
     var popup = document.getElementById('popup');
     if (popup) {
         popup.style.display = 'none';
-        document.body.style.overflow = 'auto'; // Restore scrolling of background content
+        document.body.style.overflow = 'auto'; // Rétablit le défilement du contenu arrière-plan
+        document.removeEventListener('click', outsideClickHandler);
+    }
+}
+
+function outsideClickHandler(event) {
+    var popup = document.getElementById('popup');
+    var popupContent = document.getElementById('popup-content');
+
+    if (popup && popupContent && !popupContent.contains(event.target)) {
+        closePopup();
     }
 }
